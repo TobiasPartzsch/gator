a24235fc32c323d5803da1537c77f566ca7d208a
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-07-06 12:25:29 +0200

    handler functionality to add feeds

--- handler_rss.go
+++ handler_rss.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tobiaspartzsch/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -12,4 +19,50 @@ func handlerAgg(s *state, cmd command) error {
 	}
 	fmt.Printf("Feed: %+v\n", feed)
 	return nil
+}
+
+func handlerAddfeed(s *state, cmd command) error {
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("login needs exactly two arguments (user and address)")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.config.CurrentUserName)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Fprintf(os.Stderr, "user %s does not exist\n", s.config.CurrentUserName)
+			os.Exit(1)
+		}
+		return fmt.Errorf("error querying user: %w", err)
+	}
+
+	name := cmd.args[0]
+	url := cmd.args[1]
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		Name:      name,
+		Url:       url,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed: %w", err)
+	}
+
+	fmt.Println("Feed created successfully:")
+	printFeed(feed)
+	fmt.Println()
+	fmt.Println("=====================================")
+
+	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
 }
