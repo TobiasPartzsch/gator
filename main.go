99f062e8e3e261be4816465df8eea70f3a5d9b0b
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-07-06 14:06:33 +0200

    registered unfollow command

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	cmds.register("addfeed", middlewareLoggedIn(handlerAddfeed))
 	cmds.register("feeds", handlerListFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
 
 	if len(os.Args) < 2 {
