25e1135104117a2085bc1cfe1cc687522f03bf00
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-07-07 10:28:08 +0200

    adapted our sqlc layer to last_fetched_at

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,6 +1,6 @@
 -- name: CreateFeed :one
-INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
-VALUES (@id, @created_at, @updated_at, @name, @url, @user_id)
+INSERT INTO feeds (id, created_at, updated_at, last_fetched_at, name, url, user_id)
+VALUES (@id, @created_at, @updated_at, @last_fetched_at, @name, @url, @user_id)
 RETURNING *;
 
 -- name: DeleteFeeds :exec
@@ -17,3 +17,15 @@ JOIN users ON feeds.user_id = users.id;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE url = $1;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST, id ASC
+LIMIT 1;
